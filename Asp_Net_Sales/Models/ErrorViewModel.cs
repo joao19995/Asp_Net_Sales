3065ce6738b4b56c639c52a0e252abe784b8baf2
João Brandão <joaobrandao.dev@gmail.com>
2019-01-31 11:05:50 +0000

    Add project files.

---
+++ Asp_Net_Sales/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Asp_Net_Sales.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
